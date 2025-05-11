f82a535d2ca2b70cd0827799c84dfb33b421d946
ConnorBP <connor.postma@gmail.com>
2025-05-11 16:05:41 -0400

    fixed discrepency between header and class definition for cs_gc_client

--- game/client/cstrike15/cs_gc_client.h
+++ game/client/cstrike15/cs_gc_client.h
@@ -11,12 +11,18 @@
 #include <gc_clientsystem.h> // base GCClient
 #include "cstrike15_gcmessages.pb.h"
 
+#if _MSC_VER >= 1300
+#pragma warning(disable : 4511)	// Disable warnings about private copy constructors
+#pragma warning(disable : 4121)	// warning C4121: 'symbol' : alignment of a member was sensitive to packing
+#pragma warning(disable : 4530)	// warning C4530: C++ exception handler used, but unwind semantics are not enabled. Specify /EHsc (disabled due to std headers having exception syntax)
+#endif
+
 class CCSGCClientSystem : public CGCClientSystem//, public GCSDK::ISharedObjectListener
 {
     DECLARE_CLASS_GAMEROOT( CCSGCClientSystem, CGCClientSystem );
 public:
     CCSGCClientSystem( void );
-    ~CCSGCClientSystem( void );
+    ~CCSGCClientSystem( void ) noexcept;
 
     // CAutoGameSystemPerFrame
     virtual bool Init() OVERRIDE;
