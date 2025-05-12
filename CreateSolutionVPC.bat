4bb6ac2f961967f7ff16674e3aa84126eb73e71b
ConnorBP <connor.postma@gmail.com>
2025-05-12 07:25:28 -0400

    added a registry entry fix for vpc and aded legacy warning to CreateSolutionVPC

--- CreateSolutionVPC.bat
+++ CreateSolutionVPC.bat
@@ -1,2 +1,3 @@
+REM This batch script is left over from the original build system. Prefer using CMAKE 3.15+ via buiild_with_logs.bat
 devtools\bin\vpc.exe /csgo +csgo_partner /mksln csgo_partner.sln
 pause
